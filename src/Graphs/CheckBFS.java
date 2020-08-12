146e1fb71af86bbd4b825f6cbc04294867506eac
EricZhuang9615 <xiaofzhuang@gmail.com>
2020-08-12 17:11:48 +0800

    8/12 commit

---
+++ src/Graphs/CheckBFS.java
@@ -0,0 +1,62 @@
+package Graphs;
+
+public class CheckBFS {
+    //Breadth First Traversal of Graph g
+    public static String bfs(Graph g) {
+        String result = "";
+
+        // Write - Your - Code
+        boolean[] isVisited = new boolean[g.vertices];
+        for(int i = 0; i < g.vertices; i++){
+            result += bfs("", g, int source, g.adjacencyList[i].getHeadNode(), isVisited);
+        }
+        return result;
+    }
+
+    public static String bfs(String result, Graph g, int source, DoublyLinkedList.Node root, boolean[] isVisited){
+        String temp = "";
+        if()
+        if(root == null){
+            return "";
+        }
+        if(isVisited[(int)root.data]){
+            return "";
+        }
+        DoublyLinkedList.Node curr = root;
+        while(curr != null){
+            if(!result.contains(String.valueOf(curr.data)) && !temp.contains(String.valueOf(curr.data)))
+                temp += curr.data;
+            curr = curr.nextNode;
+        }
+        result += temp;
+        curr = root;
+        while(curr != null){
+            isVisited[(int)root.data] = true;
+            temp = bfs("", g, g.adjacencyList[(int)root.data].getHeadNode(), isVisited);
+            result += temp;
+            curr = curr.nextNode;
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Graph g = new Graph(5);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        System.out.println("Graph1:");
+        g.printGraph();
+        System.out.println("BFS traversal of Graph1 : " + bfs(g));
+        System.out.println();
+
+        Graph g2 = new Graph(5);
+        g2.addEdge(0,1);
+        g2.addEdge(0,4);
+        g2.addEdge(1,2);
+        g2.addEdge(3,4);
+        System.out.println("Graph2:");
+        g2.printGraph();
+        System.out.println("BFS traversal of Graph2 : " + bfs(g2));
+    }
+}
--- src/Graphs/Graph.java
+++ src/Graphs/Graph.java
@@ -1,9 +1,64 @@
 package Graphs;
 
-public class Graph<DoublyLinkedList> {
+public class Graph {
     int vertices; //Total number of vertices in graph
-    DoublyLinkedList<Integer> adjacencyList[]; //An array of linked lists to store adjacent vertices.
+    DoublyLinkedList<Integer>[] adjacencyList; //An array of linked lists to store adjacent vertices.
 
-    void printGraph(); //Prints Graph (Adjaceny List)
-    void addEdge(int source,int destination); //Adds an Edge from source vertex to destination vertex
+    public Graph(int vertices){
+        this.vertices = vertices;
+        adjacencyList = new DoublyLinkedList[vertices];
+
+        for(int i = 0; i < vertices; i++){
+            adjacencyList[i] = new DoublyLinkedList<Integer>();
+        }
+    }
+
+    public void addEdge(int source, int destination){
+        if(source >= 0 && source < vertices && destination >= 0 && destination < vertices){
+            this.adjacencyList[source].insertAtEnd(destination);
+        }
+    }
+
+    void printGraph(){
+        System.out.println(">>Adjacency List of Directed Graph<<");
+        for (int i = 0; i < vertices; i++)
+        {
+            if(adjacencyList[i]!=null){
+                System.out.print("|" + i + "| => ");
+
+                DoublyLinkedList<Integer>.Node temp = adjacencyList[i].getHeadNode();
+                while (temp != null)
+                {
+                    System.out.print("[" + temp.data + "] -> ");
+                    temp = temp.nextNode;
+                }
+                System.out.println("null");
+            }
+            else{
+
+                System.out.println("|" + i + "| => "+ "null");
+            }
+        }
+    } //Prints Graph (Adjaceny List)
+
+    /*
+    *               0
+    *              / \
+    *             v   v
+    *             1   2
+    *             \   /
+    *              v v
+    *               3
+    * */
+
+    public static void main( String args[] ) {
+        Graph g= new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+
+        g.printGraph();
+    }
 }
